2e37be078f00999bf45b67de5948b4e85de6cbce
lenkaanuradha <lenkaanuradha149@gmail.com>
2024-07-02 20:52:07 +0530

    changes2

--- controllers/users.js
+++ controllers/users.js
@@ -1,30 +1,25 @@
 import Poll from "../models/Poll.js";
 import Users from "../models/User.js";
 
-
 export const getUser = async (req, res) => {
-
   try {
     const user = await Users.findById(req.params.user_id);
-    const polls = await Poll.findById({created_by:req.params.user_id})
-    res.status(200).json({success:"true",user:user,polls:polls});;
-  } catch (error) {
-   
-    res.json({success:"false",error:error});
-  }
-};
-export const getallUsers = async (req, res) => {
- 
-  try {
-   
-  const allUsers = await Users.find({})
-  
- res.status(200).json({success:"true", allUsers:allUsers});;
-  } catch (error) {
-   
+    if (!user) {
+      return res.status(404).json({ success: "false", error: "User not found" });
+    }
     
-    res.status(500).json({success:"false", error:error});
+    const polls = await Poll.find({ created_by: req.params.user_id });
+    res.status(200).json({ success: "true", user: user, polls: polls });
+  } catch (error) {
+    res.status(500).json({ success: "false", error: error.message });
   }
 };
 
-
+export const getallUsers = async (req, res) => {
+  try {
+    const allUsers = await Users.find({});
+    res.status(200).json({ success: "true", allUsers: allUsers });
+  } catch (error) {
+    res.status(500).json({ success: "false", error: error.message });
+  }
+};
